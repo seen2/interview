f9a4d789a344e92d8db4f56a6c466829a20c2deb
Lokpati Tiwari <36004515+seen2@users.noreply.github.com>
2025-02-19 16:49:05 +0000

    Adjusted Nth Add Linked List

--- 6-006-introduction-to-algorithms/data_structures/linked-list.js
+++ 6-006-introduction-to-algorithms/data_structures/linked-list.js
@@ -8,15 +8,15 @@ class Node {
 class LinkedList {
   len = 0;
   last = null;
-  head=null;
+  head = null;
   constructor(data) {
     this.head = new Node(data, null);
     this.last = this.head;
     this.len++;
   }
   addFirst(data) {
-    const temp=this.head;
-    this.head=new Node(data,temp);
+    const temp = this.head;
+    this.head = new Node(data, temp);
     this.len++;
   }
   addLast(data) {
@@ -25,27 +25,35 @@ class LinkedList {
     this.len++;
   }
   addNth(data, n) {
-    let nthNode = this.head;
-    while (n > 1) {
-      nthNode = nthNode.next;
-      n--;
+    if (n == 0) {
+      this.addFirst(data);
+    } else if (n == this.len - 1) {
+      this.addLast(data);
+    } else if(n>0 && n<this.len-1){
+      let nthNode = this.head;
+      while (n > 1) {
+        nthNode = nthNode.next;
+        n--;
+      }
+      const temp = nthNode.next;
+      nthNode.next = new Node(data, temp);
+      this.len++;
+    }else{
+      console.error("Out of bound");
     }
-    const temp = nthNode.next;
-    nthNode.next = new Node(data, temp);
-    this.len++;
   }
-  show(){
-    let i=0;
-    let node=this.head;
-    while(i<this.len){
-      console.log(node.data);
-      node=node.next;
+  show() {
+    let i = 0;
+    let node = this.head;
+    while (i < this.len) {
+      console.log(i,"-->",node.data);
+      node = node.next;
       i++;
     }
   }
 }
 
-let list=new LinkedList(1);
+let list = new LinkedList(1);
 list.addLast(2);
 list.addLast(4);
 list.addLast(5);
@@ -54,7 +62,7 @@ list.addFirst(0);
 console.log("--------------------");
 list.show();
 console.log("--------------------");
-list.addNth(3,3);
+list.addNth(3, 3);
 list.show();
 console.log("--------------------");
 list.addLast(6);
@@ -62,5 +70,9 @@ list.show();
 console.log("--------------------");
 list.addLast(7);
 list.show();
-console.log("--------------------");
+console.log("-----------Check---------");
+list.addNth(-1,0);
+list.addNth(9,list.len-1);
+list.show();
 console.log(list.len);
+list.addNth(10,list.len);
